86fd6e96a05f61dbcb8053315c49361d10ce2cde
Michael Slevin <michaelslevin11@gmail.com>
2016-04-25 01:27:01 -0700

    changes to admin page. going to bed.

--- coffee_site/coffee/admin.py
+++ coffee_site/coffee/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
